c427a6af13e835ce44e550a65653d0b9088ea94f
PavkaBrika <pavlikbrichkin@mail.ru>
2022-07-08 15:56:17 +0500

    changed layout for optimisation for small screens, fixed bug with edit debt

--- app/src/main/java/com/breckneck/debtbook/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/breckneck/debtbook/presentation/activity/MainActivity.kt
@@ -2,8 +2,13 @@ package com.breckneck.debtbook.presentation.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE
 import androidx.fragment.app.FragmentTransaction
+import androidx.transition.Scene
+import androidx.transition.Transition
+import androidx.transition.TransitionInflater
+import androidx.transition.TransitionManager
 import com.breckneck.debtbook.R
 import com.breckneck.debtbook.presentation.fragment.DebtDetailsFragment
 import com.breckneck.debtbook.presentation.fragment.MainFragment
@@ -12,11 +17,22 @@ import com.breckneck.deptbook.domain.model.DebtDomain
 
 class MainActivity : AppCompatActivity(), MainFragment.OnButtonClickListener, NewDebtFragment.OnButtonClickListener, DebtDetailsFragment.OnButtonClickListener {
 
+//    private lateinit var scene1: Scene
+//    private lateinit var scene2: Scene
+//    private lateinit var currentScene: Scene
+//    private lateinit var transiction: Transition
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+//        val container: FragmentContainerView = findViewById(R.id.frameLayout)
+//        scene1 = Scene.getSceneForLayout(container , R.layout.fragment_main, this)
+//        scene2 = Scene.getSceneForLayout(container , R.layout.fragment_debt_details, this)
+//
+//        scene1.enter()
+//        currentScene = scene1
+//
+//        transiction = TransitionInflater.from(this).inflateTransition(R.transition.slide_right)
 
         val fragmentManager = supportFragmentManager
         val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
@@ -24,9 +40,17 @@ class MainActivity : AppCompatActivity(), MainFragment.OnButtonClickListener, Ne
     }
 //MainFragment interfaces
     override fun OnHumanClick(idHuman: Int, currency: String) {
+//        if (currentScene === scene1) {
+//            TransitionManager.go(scene2, transiction)
+//            currentScene = scene2
+//        } else {
+//            TransitionManager.go(scene1, transiction)
+//            currentScene = scene1
+//        }
         val fragmentManager = supportFragmentManager
         val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
 //            .setCustomAnimations(R.anim.slide_in, R.anim.fade_out, R.anim.fade_in, R.anim.slide_out)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
         val args = Bundle()
         args.putInt("idHuman", idHuman)
         args.putString("currency", currency)
@@ -37,7 +61,7 @@ class MainActivity : AppCompatActivity(), MainFragment.OnButtonClickListener, Ne
 
     override fun OnAddButtonClick() {
         val fragmentManager = supportFragmentManager
-        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
 //            .setCustomAnimations(R.anim.slide_in, R.anim.fade_out, R.anim.fade_in, R.anim.slide_out)
 ////            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
 
@@ -46,7 +70,7 @@ class MainActivity : AppCompatActivity(), MainFragment.OnButtonClickListener, Ne
 //NewDebtFragment interfaces
     override fun DebtDetailsNewHuman(currency: String) {
         val fragmentManager = supportFragmentManager
-        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
 //            .setCustomAnimations(R.anim.slide_in, R.anim.fade_out, R.anim.fade_in, R.anim.slide_out)
         supportFragmentManager.popBackStack("secondary",  POP_BACK_STACK_INCLUSIVE)
         val args = Bundle()
@@ -59,7 +83,7 @@ class MainActivity : AppCompatActivity(), MainFragment.OnButtonClickListener, Ne
 
     override fun DebtDetailsExistHuman(idHuman: Int, currency: String) {
         val fragmentManager = supportFragmentManager
-        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
 //            .setCustomAnimations(R.anim.slide_in, R.anim.fade_out, R.anim.fade_in, R.anim.slide_out)
         supportFragmentManager.popBackStack("secondary",  POP_BACK_STACK_INCLUSIVE)
         val args = Bundle()
@@ -72,7 +96,7 @@ class MainActivity : AppCompatActivity(), MainFragment.OnButtonClickListener, Ne
 //DebtDetailsFragment interfaces
     override fun addNewDebtFragment(idHuman: Int, currency: String) {
         val fragmentManager = supportFragmentManager
-        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
 //            .setCustomAnimations(R.anim.slide_in, R.anim.fade_out, R.anim.fade_in, R.anim.slide_out)
         val args = Bundle()
         args.putInt("idHuman", idHuman)
@@ -82,14 +106,17 @@ class MainActivity : AppCompatActivity(), MainFragment.OnButtonClickListener, Ne
         fragmentTransaction.replace(R.id.frameLayout, fragment).addToBackStack("secondary").commit()
     }
 
-    override fun editDebt(debtDomain: DebtDomain) {
+    override fun editDebt(debtDomain: DebtDomain, currency: String) {
         val fragmentManager = supportFragmentManager
-        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
 //            .setCustomAnimations(R.anim.slide_in, R.anim.fade_out, R.anim.fade_in, R.anim.slide_out)
         val args = Bundle()
         args.putInt("idDebt", debtDomain.id)
         args.putInt("idHuman", debtDomain.idHuman)
         args.putDouble("sum", debtDomain.sum)
+        args.putString("date", debtDomain.date)
+        args.putString("info", debtDomain.info)
+        args.putString("currency", currency)
         val fragment = NewDebtFragment()
         fragment.arguments = args
         fragmentTransaction.replace(R.id.frameLayout, fragment).addToBackStack("secondary").commit()
