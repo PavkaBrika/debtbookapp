74c3c7457ca1cc2b5a70c5701e58d3c3e637b5fb
PavkaBrika <pavlikbrichkin@mail.ru>
2022-07-21 20:11:31 +0500

    fixed bug with horizontal landscape, ads sdk commented

--- app/src/main/java/com/breckneck/debtbook/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/breckneck/debtbook/presentation/activity/MainActivity.kt
@@ -1,52 +1,268 @@
 package com.breckneck.debtbook.presentation.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE
 import androidx.fragment.app.FragmentTransaction
 import com.breckneck.debtbook.R
+import com.breckneck.debtbook.presentation.MainViewModel
 import com.breckneck.debtbook.presentation.fragment.DebtDetailsFragment
 import com.breckneck.debtbook.presentation.fragment.MainFragment
 import com.breckneck.debtbook.presentation.fragment.NewDebtFragment
+import com.breckneck.deptbook.data.storage.repository.AdRepositoryImpl
+import com.breckneck.deptbook.data.storage.sharedprefs.SharedPrefsAdStorageImpl
+import com.breckneck.deptbook.domain.model.DebtDomain
+import com.breckneck.deptbook.domain.usecase.Ad.AddClickUseCase
+import com.breckneck.deptbook.domain.usecase.Ad.GetClicksUseCase
+import com.breckneck.deptbook.domain.usecase.Ad.SetClicksUseCase
+import com.yandex.mobile.ads.banner.AdSize
+import com.yandex.mobile.ads.banner.BannerAdEventListener
+import com.yandex.mobile.ads.banner.BannerAdView
+import com.yandex.mobile.ads.common.*
+import com.yandex.mobile.ads.interstitial.InterstitialAd
+import com.yandex.mobile.ads.interstitial.InterstitialAdEventListener
 
 class MainActivity : AppCompatActivity(), MainFragment.OnButtonClickListener, NewDebtFragment.OnButtonClickListener, DebtDetailsFragment.OnButtonClickListener {
+
+    private lateinit var interstitialAd: InterstitialAd
+
+    lateinit var sharedPrefsAdStorage: SharedPrefsAdStorageImpl
+    lateinit var adRepository: AdRepositoryImpl
+    lateinit var getClicks: GetClicksUseCase
+    lateinit var addClick: AddClickUseCase
+    lateinit var setClick: SetClicksUseCase
+
+    private lateinit var vm: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.e("TAG", "Activity created")
+        vm = MainViewModel()
+
+
+
+
+
+//        YANDEX MOBILE ADVERTISMENT
+//
+//        sharedPrefsAdStorage = SharedPrefsAdStorageImpl(context = this)
+//        adRepository = AdRepositoryImpl(adStorage = sharedPrefsAdStorage)
+//        getClicks = GetClicksUseCase(adRepository = adRepository)
+//        addClick = AddClickUseCase(adRepository = adRepository)
+//        setClick = SetClicksUseCase(adRepository = adRepository)
+//
+//
+//        MobileAds.initialize(this, object: InitializationListener {
+//            override fun onInitializationCompleted() {
+//                Log.e("TAG", "Yandex initialized")
+//            }
+//        })
+//        MobileAds.setUserConsent(false)
+//        //BANNER AD
+//        val bannerAd: BannerAdView = findViewById(R.id.bannerAdView)
+//        val adRequestBuild = AdRequest.Builder().build()
+//        bannerAd.apply {
+//            //setAdUnitId("R-M-1753297-1")
+//            setAdUnitId("R-M-DEMO-320x50")
+//            setAdSize(AdSize.flexibleSize(320, 50))
+//            setBannerAdEventListener(object : BannerAdEventListener{
+//                override fun onAdLoaded() {
+//                    Log.e("TAG", "BANNER LOADED")
+//                }
+//
+//                override fun onAdFailedToLoad(p0: AdRequestError) {
+//                    Log.e("TAG", "BANNER LOAD FAILED")
+//                    loadAd(adRequestBuild)
+//                }
+//
+//                override fun onAdClicked() {
+//                    Log.e("TAG", "BANNER CLICKED")
+//                }
+//
+//                override fun onLeftApplication() {
+//                    Log.e("TAG", "BANNER LEFT")
+//                }
+//
+//                override fun onReturnedToApplication() {
+//                    Log.e("TAG", "BANNER RETURN")
+//                }
+//
+//                override fun onImpression(p0: ImpressionData?) {
+//                    Log.e("TAG", "BANNER IMPRESSION")
+//                    loadAd(adRequestBuild)
+//                }
+//
+//            })
+//            loadAd(adRequestBuild)
+//        }
+//
+//        //INTERSTITIAL AD
+//        interstitialAd = InterstitialAd(this)
+//        interstitialAd.apply {
+//            setAdUnitId("R-M-1753297-2")
+//            setInterstitialAdEventListener(object : InterstitialAdEventListener {
+//                override fun onAdLoaded() {
+//                    Log.e("TAG", "INTERSTITIAL LOADED")
+//                }
+//
+//                override fun onAdFailedToLoad(p0: AdRequestError) {
+//                    Log.e("TAG", "INTERSTITIAL LOAD FAILED")
+//                    loadAd(adRequestBuild)
+//                }
+//
+//                override fun onAdShown() {
+//                    Log.e("TAG", "INTERSTITIAL SHOWN")
+//                }
+//
+//                override fun onAdDismissed() {
+//                    Log.e("TAG", "INTERSTITIAL DISMISSED")
+//                }
+//
+//                override fun onAdClicked() {
+//                    Log.e("TAG", "INTERSTITIAL CLICKED")
+//                }
+//
+//                override fun onLeftApplication() {
+//                    Log.e("TAG", "INTERSTITIAL LEFT APP")
+//                }
+//
+//                override fun onReturnedToApplication() {
+//                    Log.e("TAG", "INTERSTITIAL RETURN APP")
+//                }
+//
+//                override fun onImpression(p0: ImpressionData?) {
+//                    Log.e("TAG", "INTERSTITIAL IMPRESSION")
+//                    loadAd(adRequestBuild)
+//                }
+//            })
+//            loadAd(adRequestBuild)
+//        }
+
         val fragmentManager = supportFragmentManager
         val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frameLayout, MainFragment()).commit()
     }
-
-    override fun changeMainFragment(Fragment: String, idHuman: Int) {
+//MainFragment interfaces
+    override fun OnHumanClick(idHuman: Int, currency: String, name: String) {
         val fragmentManager = supportFragmentManager
-        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction().setCustomAnimations(R.anim.slide_in, R.anim.fade_out, R.anim.fade_in, R.anim.slide_out)
-        if (Fragment == "NewDebtFragment")
-            fragmentTransaction.replace(R.id.frameLayout, NewDebtFragment()).addToBackStack(null).commit()
-        else if (Fragment == "DebtDetailsFragment") {
-            val args = Bundle()
-            args.putInt("idHuman", idHuman)
-            val fragment = DebtDetailsFragment()
-            fragment.arguments = args
-            fragmentTransaction.replace(R.id.frameLayout, fragment).addToBackStack(null).commit()
-        }
-    }
-
-    override fun changeNewDebtFragmentToDebtDetailsFragment() {
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frameLayout, MainFragment()).addToBackStack(null).commit()
-    }
-
-    override fun addNewDebtFragment(idHuman: Int) {
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
         val args = Bundle()
         args.putInt("idHuman", idHuman)
+        args.putString("currency", currency)
+        args.putString("name", name)
+        val fragment = DebtDetailsFragment()
+        fragment.arguments = args
+        fragmentTransaction.replace(R.id.frameLayout, fragment).addToBackStack("main").commit()
+        addClick.execute()
+        if (getClicks.execute())
+            if (interstitialAd.isLoaded) {
+                interstitialAd.show()
+                setClick.execute()
+            }
+    }
+
+    override fun OnAddButtonClick() {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+        fragmentTransaction.replace(R.id.frameLayout, NewDebtFragment()).addToBackStack("main").commit()
+        addClick.execute()
+        if (getClicks.execute())
+            if (interstitialAd.isLoaded) {
+                interstitialAd.show()
+                setClick.execute()
+            }
+    }
+//NewDebtFragment interfaces
+    override fun DebtDetailsNewHuman(currency: String, name: String) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+        supportFragmentManager.popBackStack("secondary",  POP_BACK_STACK_INCLUSIVE)
+        val args = Bundle()
+        args.putBoolean("newHuman", true)
+        args.putString("currency", currency)
+        args.putString("name", name)
+        val fragment = DebtDetailsFragment()
+        fragment.arguments = args
+        fragmentTransaction.replace(R.id.frameLayout, fragment).commit()
+        addClick.execute()
+        if (getClicks.execute())
+            if (interstitialAd.isLoaded) {
+                interstitialAd.show()
+                setClick.execute()
+            }
+    }
+
+    override fun DebtDetailsExistHuman(idHuman: Int, currency: String, name: String) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+        supportFragmentManager.popBackStack("secondary",  POP_BACK_STACK_INCLUSIVE)
+        val args = Bundle()
+        args.putInt("idHuman", idHuman)
+        args.putString("currency", currency)
+        args.putString("name", name)
+        val fragment = DebtDetailsFragment()
+        fragment.arguments = args
+        fragmentTransaction.replace(R.id.frameLayout, fragment).commit()
+        addClick.execute()
+        if (getClicks.execute())
+            if (interstitialAd.isLoaded) {
+                interstitialAd.show()
+                setClick.execute()
+            }
+    }
+//DebtDetailsFragment interfaces
+    override fun addNewDebtFragment(idHuman: Int, currency: String, name: String) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+        val args = Bundle()
+        args.putInt("idHuman", idHuman)
+        args.putString("currency", currency)
+        args.putString("name", name)
         val fragment = NewDebtFragment()
         fragment.arguments = args
-        fragmentTransaction.replace(R.id.frameLayout, fragment).addToBackStack(null).commit()
+        fragmentTransaction.replace(R.id.frameLayout, fragment).addToBackStack("secondary").commit()
+        addClick.execute()
+        if (getClicks.execute())
+            if (interstitialAd.isLoaded) {
+                interstitialAd.show()
+                setClick.execute()
+            }
+    }
+
+    override fun editDebt(debtDomain: DebtDomain, currency: String, name: String) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+        val args = Bundle()
+        args.putInt("idDebt", debtDomain.id)
+        args.putInt("idHuman", debtDomain.idHuman)
+        args.putDouble("sum", debtDomain.sum)
+        args.putString("date", debtDomain.date)
+        args.putString("info", debtDomain.info)
+        args.putString("name", name)
+        args.putString("currency", currency)
+        val fragment = NewDebtFragment()
+        fragment.arguments = args
+        fragmentTransaction.replace(R.id.frameLayout, fragment).addToBackStack("secondary").commit()
+        addClick.execute()
+        if (getClicks.execute())
+            if (interstitialAd.isLoaded) {
+                interstitialAd.show()
+                setClick.execute()
+            }
+    }
+
+    override fun deleteHuman() {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, MainFragment()).commit()
+        addClick.execute()
+        if (getClicks.execute())
+            if (interstitialAd.isLoaded) {
+                interstitialAd.show()
+                setClick.execute()
+            }
     }
 }
--- app/src/main/java/com/breckneck/debtbook/presentation/fragment/DebtDetailsFragment.kt
+++ app/src/main/java/com/breckneck/debtbook/presentation/fragment/DebtDetailsFragment.kt
@@ -1,27 +1,48 @@
 package com.breckneck.debtbook.presentation.fragment
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
+import android.graphics.Typeface
 import android.os.Bundle
+import android.transition.TransitionInflater
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import com.breckneck.debtbook.R
 import com.breckneck.debtbook.adapter.DebtAdapter
 import com.breckneck.deptbook.data.storage.database.DataBaseDebtStorageImpl
+import com.breckneck.deptbook.data.storage.database.DataBaseHumanStorageImpl
 import com.breckneck.deptbook.data.storage.repository.DebtRepositoryImpl
+import com.breckneck.deptbook.data.storage.repository.HumanRepositoryImpl
+import com.breckneck.deptbook.domain.model.DebtDomain
+import com.breckneck.deptbook.domain.usecase.Debt.DeleteDebtUseCase
+import com.breckneck.deptbook.domain.usecase.Debt.DeleteDebtsByHumanIdUseCase
 import com.breckneck.deptbook.domain.usecase.Debt.GetAllDebtsUseCase
+import com.breckneck.deptbook.domain.usecase.Human.*
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
 
 class DebtDetailsFragment: Fragment() {
 
     interface OnButtonClickListener{
-        fun addNewDebtFragment(idHuman: Int)
+        fun addNewDebtFragment(idHuman: Int, currency: String, name: String)
+
+        fun editDebt(debtDomain: DebtDomain, currency: String, name: String)
+
+        fun deleteHuman()
     }
 
     var buttonClickListener: OnButtonClickListener? = null
@@ -31,43 +52,175 @@ class DebtDetailsFragment: Fragment() {
         buttonClickListener = context as OnButtonClickListener
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val inflater = TransitionInflater.from(requireContext())
+        enterTransition = inflater.inflateTransition(R.transition.slide_right)
+    }
+
+    lateinit var dataBaseHumanStorage: DataBaseHumanStorageImpl
+    lateinit var humanRepository: HumanRepositoryImpl
+    lateinit var getLastHumanId: GetLastHumanIdUseCase
+    lateinit var addSumUseCase: AddSumUseCase
+    lateinit var getHumanSumDebt: GetHumanSumDebtUseCase
+    lateinit var deleteHuman: DeleteHumanUseCase
+
+    lateinit var dataBaseDebtStorage: DataBaseDebtStorageImpl
+    lateinit var debtRepository: DebtRepositoryImpl
+    lateinit var getAllDebts: GetAllDebtsUseCase
+    lateinit var deleteDebt: DeleteDebtUseCase
+    lateinit var deleteDebtsByHumanId: DeleteDebtsByHumanIdUseCase
+
+    lateinit var debtClickListener: DebtAdapter.OnDebtClickListener
+    lateinit var overallSumTextView: TextView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_debt_details, container, false)
 
-        val dataBaseDebtStorage = DataBaseDebtStorageImpl(context = view.context)
-        val debtRepository = DebtRepositoryImpl(debtStorage = dataBaseDebtStorage)
-        val getAllDebtsUseCase = GetAllDebtsUseCase(debtRepository = debtRepository)
+        dataBaseHumanStorage = DataBaseHumanStorageImpl(context = view.context)
+        humanRepository = HumanRepositoryImpl(humanStorage = dataBaseHumanStorage)
+        getLastHumanId = GetLastHumanIdUseCase(humanRepository = humanRepository)
+        addSumUseCase = AddSumUseCase(humanRepository = humanRepository)
+        getHumanSumDebt = GetHumanSumDebtUseCase(humanRepository = humanRepository)
+        deleteHuman = DeleteHumanUseCase(humanRepository = humanRepository)
+
+        dataBaseDebtStorage = DataBaseDebtStorageImpl(context = view.context)
+        debtRepository = DebtRepositoryImpl(debtStorage = dataBaseDebtStorage)
+        getAllDebts = GetAllDebtsUseCase(debtRepository = debtRepository)
+        deleteDebt = DeleteDebtUseCase(debtRepository = debtRepository)
+        deleteDebtsByHumanId = DeleteDebtsByHumanIdUseCase(debtRepository = debtRepository)
 
         val recyclerView: RecyclerView = view.findViewById(R.id.debtsRecyclerView)
+        recyclerView.addItemDecoration(DividerItemDecoration(view.context, DividerItemDecoration.VERTICAL))
 
-        val idHuman = arguments?.getInt("idHuman", 0)
+        var idHuman = arguments?.getInt("idHuman", 0)
+        val newHuman = arguments?.getBoolean("newHuman", false)
+        val currency = arguments?.getString("currency")
+        val name = arguments?.getString("name")
+
+        val collaps: CollapsingToolbarLayout = view.findViewById(R.id.collaps)
+        collaps.title = name
+        collaps.apply {
+            setCollapsedTitleTypeface(Typeface.DEFAULT_BOLD)
+            setExpandedTitleTypeface(Typeface.DEFAULT_BOLD)
+        }
+
+        val actions = arrayOf(getString(R.string.deletedebt), getString(R.string.editdebt))
+        debtClickListener = object : DebtAdapter.OnDebtClickListener{ //ALERT DIALOG
+            override fun onDebtClick(debtDomain: DebtDomain, position: Int) {
+                val builder = AlertDialog.Builder(view.context)
+                builder.setItems(actions) {dialog, which ->
+                    if (actions[which] == getString(R.string.deletedebt)) { //DELETE DEBT
+                        Single.just(actions)
+                            .map {
+                                deleteDebt.execute(debtDomain)
+                                addSumUseCase.execute(humanId = debtDomain.idHuman, sum = (debtDomain.sum * (-1.0)))
+                                setOverallSumText(sum = getHumanSumDebt.execute(idHuman!!), currency = currency!!, view = view)
+                                Log.e("TAG", "Debt delete success")
+                                return@map getAllDebts.execute(id = idHuman!!)
+                            }
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe({
+                                val adapter = DebtAdapter(it, debtClickListener, currency!!)
+                                recyclerView.adapter = adapter
+                                Log.e("TAG", "Debts load success")
+                            },{})
+                    } else { //EDIT DEBT
+                        buttonClickListener?.editDebt(debtDomain = debtDomain, currency = currency!!, name = name!!)
+                    }
+                }
+                builder.show()
+                Log.e("TAG", "Click on debt with id = ${debtDomain.id}")
+            }
+        }
 
         Single.just("1")
             .map {
-                return@map getAllDebtsUseCase.execute(id = idHuman!!)
+                if (newHuman == true) {
+                    idHuman = getLastHumanId.exectute()
+                }
+                Log.e("TAG", "Open Debt Details of Human id = $idHuman")
+                return@map getAllDebts.execute(id = idHuman!!)
             }
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
-                val adapter = DebtAdapter(view.context, it)
+                val adapter = DebtAdapter(it, debtClickListener, currency!!)
                 recyclerView.adapter = adapter
                 Log.e("TAG", "Debts load success")
             }, {
 
             })
 
-        Single.just("1")
+        val overallSumTextView: TextView = view.findViewById(R.id.overallSumTextView)
+        Single.just(overallSumTextView)
             .map {
-
+                if (newHuman == true) {
+                    idHuman = getLastHumanId.exectute()
+                }
+                return@map getHumanSumDebt.execute(idHuman!!)
             }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                setOverallSumText(sum = it, currency = currency!!, view = view)
+            }, {
+            })
 
-        val addDebtButton: Button = view.findViewById(R.id.addDebtButton)
+        val deleteHumanButton: ImageView = view.findViewById(R.id.deleteHumanButton)
+        deleteHumanButton.setOnClickListener {
+            val builder = AlertDialog.Builder(view.context)
+            builder.setTitle(R.string.payoffTitle)
+            builder.setMessage(R.string.payoffMessage)
+            builder.setPositiveButton(R.string.yes, object: DialogInterface.OnClickListener {
+                override fun onClick(p0: DialogInterface?, p1: Int) {
+                    Single.just(deleteHumanButton)
+                        .map {
+                            if (newHuman == true) {
+                                idHuman = getLastHumanId.exectute()
+                            }
+                            deleteHuman.execute(id = idHuman!!)
+                            deleteDebtsByHumanId.execute(id = idHuman!!)
+                        }
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe({
+                            Log.e("TAG", "Human with id = $idHuman deleted")
+                        },{})
+                    buttonClickListener?.deleteHuman()
+                }
+            })
+            builder.setNegativeButton(R.string.No, null)
+            builder.show()
+        }
+
+        val addDebtButton: FloatingActionButton = view.findViewById(R.id.addDebtButton)
         addDebtButton.setOnClickListener{
             if (idHuman != null) {
-                buttonClickListener?.addNewDebtFragment(idHuman = idHuman)
+                buttonClickListener?.addNewDebtFragment(idHuman = idHuman!!, currency = currency!!, name = name!!)
             }
         }
 
         return view
+    }
+
+    fun setOverallSumText(sum: Double, currency: String, view: View) {
+        overallSumTextView = view.findViewById(R.id.overallSumTextView)
+        val decimalFormat = DecimalFormat("###,###,###.##")
+        val customSymbol: DecimalFormatSymbols = DecimalFormatSymbols()
+        customSymbol.groupingSeparator = ' '
+        decimalFormat.decimalFormatSymbols = customSymbol
+        if (sum > 0) {
+            overallSumTextView.setTextColor(ContextCompat.getColor(view.context, R.color.green))
+            overallSumTextView.text = "+${decimalFormat.format(sum)} $currency"
+        }
+        else if (sum < 0) {
+            overallSumTextView.setTextColor(ContextCompat.getColor(view.context, R.color.red))
+            overallSumTextView.text = "${decimalFormat.format(sum)} $currency"
+        } else {
+            overallSumTextView.setTextColor(ContextCompat.getColor(view.context, R.color.darkgray))
+            overallSumTextView.text = "${decimalFormat.format(sum)} $currency"
+        }
     }
 }
