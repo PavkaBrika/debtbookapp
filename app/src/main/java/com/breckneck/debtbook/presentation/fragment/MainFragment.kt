11dfed2a168ca02b1d5d0777495e11d830abb69c
PavkaBrika <pavlikbrichkin11@gmail.com>
2023-08-22 22:44:20 +0500

    Released v1.4 (5)

--- app/src/main/java/com/breckneck/debtbook/presentation/fragment/MainFragment.kt
+++ app/src/main/java/com/breckneck/debtbook/presentation/fragment/MainFragment.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import android.view.*
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.ScrollView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
