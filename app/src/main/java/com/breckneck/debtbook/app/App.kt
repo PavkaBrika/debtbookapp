cccccfc8d895fab9a5c6295d8d119ec51f42f367
PavkaBrika <pavlikbrichkin11@gmail.com>
2023-09-10 14:51:40 +0500

    updated yandex ads

--- app/src/main/java/com/breckneck/debtbook/app/App.kt
+++ app/src/main/java/com/breckneck/debtbook/app/App.kt
@@ -1,9 +1,12 @@
 package com.breckneck.debtbook.app
 
 import android.app.Application
+import android.util.Log
 import com.breckneck.debtbook.di.appModule
 import com.breckneck.debtbook.di.dataModule
 import com.breckneck.debtbook.di.domainModule
+import com.yandex.mobile.ads.common.InitializationListener
+import com.yandex.mobile.ads.common.MobileAds
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -13,6 +16,12 @@ class App: Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        MobileAds.initialize(this, object: InitializationListener {
+            override fun onInitializationCompleted() {
+                Log.e("TAG", "Yandex initialized")
+            }
+        })
 
         startKoin {
             androidLogger(Level.DEBUG)
