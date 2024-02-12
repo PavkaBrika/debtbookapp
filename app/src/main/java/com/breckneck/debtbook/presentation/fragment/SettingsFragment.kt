a2756f25edaecabf0c5792c6de6ad57b0b3b8254
PavkaBrika <pavlikbrichkin11@gmail.com>
2024-02-12 18:12:59 +0500

    added dependencies and created sign in logic in settings fragment. created new fragment for synchronization info

--- app/src/main/java/com/breckneck/debtbook/presentation/fragment/SettingsFragment.kt
+++ app/src/main/java/com/breckneck/debtbook/presentation/fragment/SettingsFragment.kt
@@ -10,11 +10,13 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.CompoundButton
 import android.widget.CompoundButton.OnCheckedChangeListener
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.Fragment
@@ -24,10 +26,21 @@ import com.breckneck.debtbook.BuildConfig
 import com.breckneck.debtbook.R
 import com.breckneck.debtbook.adapter.SettingsAdapter
 import com.breckneck.debtbook.presentation.viewmodel.SettingsFragmentViewModel
+import com.breckneck.debtbook.synchronization.DriveServiceHelper
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.Scope
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.api.client.extensions.android.http.AndroidHttp
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
+import com.google.api.client.json.gson.GsonFactory
+import com.google.api.services.drive.Drive
+import com.google.api.services.drive.DriveScopes
+import com.google.api.services.drive.model.FileList
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.Collections
 import java.util.concurrent.TimeUnit
 
 class SettingsFragment : Fragment() {
@@ -41,6 +54,11 @@ class SettingsFragment : Fragment() {
     lateinit var buttonClickListener: OnButtonClickListener
 
     private val vm by viewModel<SettingsFragmentViewModel>()
+
+    private val REQUEST_CODE_SIGN_IN = 200
+    private var mDriveServiceHelper: DriveServiceHelper? = null
+    private var fileId: String? = null
+    private val fileName = "DebtBookSync.txt"
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -264,6 +282,78 @@ class SettingsFragment : Fragment() {
         setSettingsButton.setOnClickListener {
             buttonClickListener.onBackSettingsButtonClick()
         }
+
+        val googleSignInButton: Button = view.findViewById(R.id.googleSignInButton)
+        googleSignInButton.setOnClickListener {
+            requestGoogleSignIn()
+        }
+    }
+
+    private fun findOrCreateFile(it: FileList) {
+        if (it.files.size == 0) {
+            createNewFile()
+        } else {
+            fileId = it.files.first().id
+
+            Toast.makeText(requireActivity(), "Found file for chat", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun createNewFile() {
+        mDriveServiceHelper!!.createFile(fileName)
+            .addOnSuccessListener {
+                fileId = it; Toast.makeText(
+                requireActivity(),
+                "Created file for chat",
+                Toast.LENGTH_LONG
+            ).show()
+            }
+            .addOnFailureListener { throw it }
+    }
+
+    private fun requestGoogleSignIn() {
+        Toast.makeText(requireActivity(), "Start sign in, pls wait ...", Toast.LENGTH_LONG).show()
+
+        val signInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestScopes(Scope(DriveScopes.DRIVE_APPDATA))
+            .build()
+        val client = GoogleSignIn.getClient(requireActivity(), signInOptions)
+
+        // The result of the sign-in Intent is handled in onActivityResult.
+        startActivityForResult(client.signInIntent, REQUEST_CODE_SIGN_IN)
+    }
+
+    private fun handleSignInResult(result: Intent) {
+        GoogleSignIn.getSignedInAccountFromIntent(result)
+            .addOnSuccessListener { googleAccount ->
+
+                // Use the authenticated account to sign in to the Drive service.
+                val credential = GoogleAccountCredential.usingOAuth2(
+                    requireActivity(), Collections.singleton(DriveScopes.DRIVE_APPDATA)
+                )
+                credential.selectedAccount = googleAccount.account
+                val googleDriveService =
+                    Drive.Builder(AndroidHttp.newCompatibleTransport(), GsonFactory(), credential)
+                        .setApplicationName(BuildConfig.APPLICATION_ID)
+                        .build()
+
+                // The DriveServiceHelper encapsulates all REST API and SAF functionality.
+                // Its instantiation is required before handling any onClick actions.
+                mDriveServiceHelper = DriveServiceHelper(googleDriveService)
+
+                Toast.makeText(requireActivity(), "Sign in successful", Toast.LENGTH_LONG).show()
+            }
+            .addOnFailureListener { exception -> throw exception }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            REQUEST_CODE_SIGN_IN -> {
+                handleSignInResult(data!!)
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
