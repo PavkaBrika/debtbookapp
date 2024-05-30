eb8fefa2b2b4638704004e0a79e484064065dee5
PavkaBrika <pavlikbrichkin11@gmail.com>
2024-05-30 14:47:19 +0500

    created initial files for new GoalDeposit CA logic

---
+++ data/src/main/java/com/breckneck/deptbook/data/storage/GoalDepositStorage.kt
@@ -0,0 +1,4 @@
+package com.breckneck.deptbook.data.storage
+
+interface GoalDepositStorage {
+}
---
+++ domain/src/main/java/com/breckneck/deptbook/domain/repository/GoalDepositRepository.kt
@@ -0,0 +1,4 @@
+package com.breckneck.deptbook.domain.repository
+
+interface GoalDepositRepository {
+}
