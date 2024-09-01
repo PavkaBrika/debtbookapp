737331cfbb3c48455619e7ca066ec3c4ed66d83f
PavkaBrika <pavlikbrichkin11@gmail.com>
2024-09-01 16:40:13 +0500

    created auth activity and base biometric functionality

---
+++ app/src/main/java/com/breckneck/debtbook/core/ui/theme/Color.kt
@@ -0,0 +1,6 @@
+package com.breakneck.pokedex.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LightBlue = Color(0xFFBAC7FF)
+val lightGrey = Color(0xFFAAAAAA)
---
+++ app/src/main/java/com/breckneck/debtbook/core/ui/theme/Theme.kt
@@ -0,0 +1,40 @@
+package com.breakneck.pokedex.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColorScheme(
+    primary = Color.Yellow,
+    background = Color(0xFF101010),
+    onBackground = Color.White,
+    surface = Color(0xFF303030),
+    onSurface = Color.White
+)
+
+private val LightColorPalette = lightColorScheme(
+    primary = Color.Blue,
+    background = LightBlue,
+    onBackground = Color.Black,
+    surface = Color.White,
+    onSurface = Color.Black
+)
+
+@Composable
+fun DebtBookTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
