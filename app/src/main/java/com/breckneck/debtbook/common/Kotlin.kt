897a8323c19309944c413887bf7140f05c3fecba
Pavel Brichkin <pavlikbrichkin@gmail.com>
2026-04-18 15:42:49 +0500

    Update extension functions with patterns

--- app/src/main/java/com/breckneck/debtbook/common/Kotlin.kt
+++ app/src/main/java/com/breckneck/debtbook/common/Kotlin.kt
@@ -10,13 +10,13 @@ import java.util.Locale
 val String.Companion.empty
         get() = ""
 
-fun Date.toDMYFormat(): String {
-    val sdf = SimpleDateFormat("d MMM yyyy", Locale.getDefault())
+fun Date.toDMYFormat(pattern: String = "d MMM yyyy"): String {
+    val sdf = SimpleDateFormat(pattern, Locale.getDefault())
     return sdf.format(this)
 }
 
-fun Double.format(): String {
+fun Double.format(pattern: String = "###,###,###.##"): String {
     val symbols = DecimalFormatSymbols().apply { groupingSeparator = ' ' }
-    val df = DecimalFormat("###,###,###.##", symbols)
+    val df = DecimalFormat(pattern , symbols)
     return df.format(this)
 }
