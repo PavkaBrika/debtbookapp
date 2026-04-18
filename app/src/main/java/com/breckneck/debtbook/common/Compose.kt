a20d1e9093c0ee988251dc153c83d17738a62fb4
Pavel Brichkin <pavlikbrichkin@gmail.com>
2026-04-18 15:39:42 +0500

    Add dark preview annotation class

--- app/src/main/java/com/breckneck/debtbook/common/Compose.kt
+++ app/src/main/java/com/breckneck/debtbook/common/Compose.kt
@@ -1,2 +1,7 @@
 package com.breckneck.debtbook.common
 
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+annotation class PreviewDark
